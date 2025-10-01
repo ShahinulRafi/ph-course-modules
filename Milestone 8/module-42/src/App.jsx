220a973e0bf5ceaacc67fb86360e840c9afe110b
ShahinulRafi <shahinul.csecu@gmail.com>
2025-10-01 12:20:27 +0600

    module 44 demo codes added

--- Milestone 8/module-42/src/App.jsx
+++ Milestone 8/module-42/src/App.jsx
@@ -2,7 +2,8 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
-import Nav from "./components/nav/nav";
+import Nav from "./components/nav/Nav";
+// import Nav from "./components/nav/nav";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,23 +15,6 @@ function App() {
       </nav>
       <h1 className="text-6xl">Vite + React</h1>
 
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
--- Milestone 8/module-42/src/components/nav/Nav.jsx
+++ Milestone 8/module-42/src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "./Link";
-
+import { Menu } from "lucide-react";
 //Nav, Dynamic Nav, Nav using props
 
 const navItems = [
@@ -12,16 +12,33 @@ const navItems = [
 ];
 
 const Nav = () => {
+  const links = navItems.map((item) => <Link key={item.id} item={item}></Link>);
   return (
-    <nav>      
-      {/* Nav using props */}
-      <ul>
-        {
-          navItems.map(item =>
-              <Link key={item.id} item={item}></Link>
-          )
-        }
+    <nav>
+      {/* Mobile Nav vs Web Nav */}
+      <ul className="md:hidden flex justify-center">
+        <span className="">
+          <Menu className="mr-2"></Menu>
+          <ul className="absolute bg-green-400">{links}</ul>
+        </span>
+        <h3>My Navbar</h3>
       </ul>
+
+      <ul className="hidden md:flex md:justify-between md:mx-10">
+        <h3>My Navbar</h3>
+
+        {links}
+      </ul>
+
+      {/* Nav using props */}
+      {/* <ul className="flex justify-between mx-10">
+        <Menu></Menu>
+        <h3>My Navbar</h3>
+        
+        {navItems.map((item) => (
+          <Link key={item.id} item={item}></Link>
+        ))}
+      </ul> */}
 
       {/* Dynamic Nav */}
       {/* <ul>
