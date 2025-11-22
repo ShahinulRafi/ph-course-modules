7e43a3fd8e46d3e3f6d4ed2b1ddc70fefee69a86
ShahinulRafi <shahinul.csecu@gmail.com>
2025-11-22 18:01:02 +0600

    final project part 1

--- Milestone 11/zap-shift-client/src/Routes/Router.jsx
+++ Milestone 11/zap-shift-client/src/Routes/Router.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Home Page</div>,
+        Component: RootLayout,
+        children: [
+            {
+                index: true, 
+                Component: Home
+            }
+        ]
     },
 ])
